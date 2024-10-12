aa9f327db46efed5d4c33b896f33389357ae1814
User058 <110759720+User058@users.noreply.github.com>
2024-10-12 23:39:34 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -32,8 +32,8 @@ fun_bar() {
 res1() {
     wget https://raw.githubusercontent.com/User058/vojo/main/ubuntu/menu.zip
     unzip menu.zip
-    chmod +x menu/*
-    mv menu/* /usr/local/sbin
+    chmod +x menu
+    mv menu /usr/local/sbin
     rm -rf menu
     rm -rf menu.zip
     rm -rf update.sh
@@ -41,7 +41,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATED SCRIPT CHAPEEY STORE       \e[0m"
+echo -e " \e[1;97;101m          UPDATED USER058 VPN       \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
